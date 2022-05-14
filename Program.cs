db8b7778f2a252f1811491a38c16ff590d1b0b00
JoaoPedrocordoba <jp.8pedro862gmail.com>
2022-05-14 10:41:51 -0300

    finalizado exercicio

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿int contador = 0;
+
+Console.Write("Escolha um numero: ");
+int contador2 = Convert.ToInt32(Console.ReadLine());
+
+while (contador < contador2)
+{
+    Console.WriteLine(contador);
+    contador = contador + 2;
+}
